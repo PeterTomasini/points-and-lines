8f4dd2e9300fe8bb41a9cd61fbf30fb4ef43d919
PeterTomasini <peter.tomasini.stu@cedarpark.org>
2024-11-25 20:28:09 -0800

    line compatibility

--- Main.java
+++ Main.java
@@ -4,16 +4,11 @@ public class Main {
     }
     public void start() {
         Point p1 = new Point(1, 1);
-        Point p2 = new Point(4, 5);
+        Point p2 = new Point(1, 6);
         System.out.println(p1);
         System.out.println(p2);
-        System.out.println(p1.distanceTo(p2));
-//        Point p2 = new Point();
-//        System.out.println(p2);
-//        Point p3 = new Point(1.5, 7.6);
-//        System.out.println(p3);
-//        Point p2 = new Point(0,0);
-//        p2.translate(3,4);
-//        System.out.print(p1.distanceFromTo(p2.getX(), p2.getY()));
+        Line l1 = new Line(p1, p2);
+        System.out.println("Length is " + l1.getLength());
+        System.out.println("Slope is " + l1.getSlope());
     }
 }
